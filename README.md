c257f2f25b96ba318570faa939218229038820c0
PiyushAgrawal1243 <piyush.agarwal@knoldus.com>
2021-12-09 15:50:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-qc3vyy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-qc3vyy)
---
+++ index.js
@@ -0,0 +1,25 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+
+const fb = document.getElementById('fb');
+fb.addEventListener('click', shareonFacebook);
+
+const tweet = document.getElementById('twitter');
+tweet.addEventListener('click', shareOnTwitter);
+
+function shareonFacebook() {
+  const navUrl =
+    'https://www.facebook.com/sharer/sharer.php?u=' +
+    'https://github.com/knoldus/angular-facebook-twitter.git';
+  window.open(navUrl, '_blank');
+}
+
+function shareOnTwitter() {
+  const navUrl =
+    'https://twitter.com/intent/tweet?text=' +
+    'https://github.com/knoldus/angular-facebook-twitter.git';
+  window.open(navUrl, '_blank');
+}
